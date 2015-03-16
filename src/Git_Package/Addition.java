4311da8ae7f71227370673b3dff15b0c23458c88
pppatil <preetiprashantpatil@gmail.com>
2015-03-16 15:13:50 -0700

    First Java Program

---
+++ src/Git_Package/Addition.java
@@ -0,0 +1,17 @@
+package Git_Package;
+
+public class Addition {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+      int A=addition(4,5);
+      System.out.println(A);
+	}
+	
+	public static int addition (int a,int b)
+	{
+		int sum=a+b;
+		return(sum);
+	}
+
+}
